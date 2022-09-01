337ce1ac6c9cac8dcf6fd2c886d8cdd17e40f483
Wilgot Wardell <wilgotwardell@hotmail.com>
2022-09-01 11:53:00 +0200

    api test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
 import './App.css';
 import CountryList from './components/CountryList';
 import MiddleBar from './components/MiddleBar';
 import TopBar from './components/TopBar';
 
+import { useState, useEffect} from 'react';
+
 function App() {
-  
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    let response = await fetch('https://restcountries.com/v3.1/all')
+    setData(await response.json())
+    console.log(data)
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
   return (
     <div className="App">
       <TopBar></TopBar>
