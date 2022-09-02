4b0f8ffcde40f04209dfda5bdf9052beb764f032
Wilgot Wardell <wilgotwardell@hotmail.com>
2022-09-02 11:38:17 +0200

    Country images

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 function Item({countries}) {
+  // countries.map((country, index) =>
+  //   let flagName = country.;
+  // );
+  
+  // let flag = "https://flagcdn.com/w320/" + flagName + ".png"
+
   return countries.map((country, index) => (
     <div className='item' key={index}>
-        <div className='country-image'>Image...</div>
+        <div className='country-image'>
+          <img src={country.flags.png} className="image" alt="" />
+        </div>
         <h1 className='country-name'>{country.name.common}</h1>
         <p className='country-description'>Description</p>
     </div>
