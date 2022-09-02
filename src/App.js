60ff78dc6f3eb2676d5a9bba3a07170fb59bacbe
Wilgot Wardell <wilgotwardell@hotmail.com>
2022-09-02 11:12:26 +0200

    search working

--- src/App.js
+++ src/App.js
@@ -8,32 +8,38 @@ import Item from './components/Item'
 import RegionSelector from './components/RegionSelector'
 
 function App() {
+  const [allCountries, setAllCountries] = useState([]);
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
 
   const changeSearch = e => { 
     setSearch(e.target.value)
+    // console.log(search);
   }
 
   const getData = async () => {
     let res = await fetch('https://restcountries.com/v3.1/all')
-    setCountries(await res.json());
+    setAllCountries(await res.json());
   }
 
   useEffect(() => {
     getData();
   }, []);
   
- 
+  
+  useEffect(() => {
+    console.log(allCountries)
+  }, [allCountries]);
   useEffect(() => {
     console.log(countries)
   }, [countries]);
 
   const searchForCountry = async() => {
-    await getData();
+    let all = allCountries;
+    setCountries(all);
     console.log("Searching for: " + search);
-    
-    let newArr = [...countries]
+  
+    let newArr = [...all]
     let filteredCountries = newArr.filter(country => country.name.common.includes(search))
     setCountries(filteredCountries);
   }
